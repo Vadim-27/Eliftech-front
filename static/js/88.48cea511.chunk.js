1eb332612bd30545a785fc4f57ad746efadcec01
Vadim-27 <108513205+Vadim-27@users.noreply.github.com>
2023-06-04 22:21:23 +0000

    Deploying to gh-pages from @ Vadim-27/Eliftech-front@96432782052e53c06e7e0065ec4530daa16309d5 🚀

---
+++ static/js/88.48cea511.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[88],{5088:function(e,a,r){r.r(a),r.d(a,{default:function(){return u}});var t="mainPage_markupPage__QIpDf",n="mainPage_title__-xhK0",s=r(3329),u=function(){return(0,s.jsx)("div",{className:t,children:(0,s.jsx)("h1",{className:n,children:"Restaurants and more, delivered to your door"})})}}}]);
+//# sourceMappingURL=88.48cea511.chunk.js.map
