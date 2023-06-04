fb14a28497ab9180d9515949d041cd6f267ebbc3
Vadim-27 <vadim.bulanyi@gmail.com>
2023-06-04 22:05:08 +0200

    rout2

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { store, persistor } from 'redux/store';
 
 
 import Navbar from 'modules/Navbar/Navbar';
-import MainPage from 'Page/MainPage/MainPage';
+// import MainPage from 'Page/MainPage/MainPage';
 import ShopPage from 'Page/ShopPage/ShopPage';
 import ShoppingCardPage from 'Page/ShoppingCardPage/ShoppingCardPage';
 import NotFoundPage from 'Page/NotFoundPage/NotFoundPage';
@@ -14,7 +14,7 @@ export const App = () => {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/">
+        <BrowserRouter>
           <Navbar />
           <Routes>
             {/* <Route path="/" element={<MainPage />} /> */}
